69f0caf781629b5188352f86c3796bfb01f1eec6
root <168447636@qq.com>
2018-09-26 09:58:33 +0800

    2

--- uwsgi/docker-build.sh
+++ uwsgi/docker-build.sh
@@ -1,1 +1,1 @@
-docker build --rm -t python:latest -f ./dockfile.python .
+docker build --rm -t python:latest -f ./dockerfile.python .
--- uwsgi/startpython.sh
+++ uwsgi/startpython.sh
@@ -1,4 +1,5 @@
 /usr/sbin/nginx -c /uwsgi/nginx.conf
+[ -d /run/uwsgi ] || mkdir /run/uwsgi
 uwsgi /uwsgi/testflk.ini
 while [[ true ]]; do 
     sleep 1 
